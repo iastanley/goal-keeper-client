77f771788728ee300018df2a4513f53f7a870346
iastanley <iasvks.craigslist@gmail.com>
2017-06-09 16:41:08 -0700

    added NavBar and Footer component files

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
 export default function Landing(props) {
   return (
-    
+    <div>
+      <NavBar/>
+      <div className="landing-body">
+
+      </div>
+      <Footer />
+    </div>
+
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+
+
+export default function NavBar(props) {
+
+    return (
+      <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Goal Keeper</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem href="#">Login</NavItem>
+              <NavItem href="#">Signup</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default withRouter(NavBar);
