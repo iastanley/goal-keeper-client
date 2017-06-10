2c3d57bc294abe6d2556e306d70e9879b3481332
iastanley <iasvks.craigslist@gmail.com>
2017-06-10 15:50:07 -0700

    added react router with root route for landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import NavBar from './NavBar';
+import Footer from './Footer';
 import Landing from './Landing';
 
 //I think the App component will render the different routes
 
-class App extends Component {
-  render() {
-    return ( <Landing/>);
-  }
+export default function App(props) {
+  return (
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/" component={Landing}/>
+      </Switch>
+      <Footer/>
+    </div>
+  );
 }
-
-export default App;
