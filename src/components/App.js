e941293fb9401cb48e73418eb31cfcb1a59245da
iastanley <iasvks.craigslist@gmail.com>
2017-06-10 17:11:58 -0700

    added pages for landing page and tasks page

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './NavBar';
 import Footer from './Footer';
-import Landing from './Landing';
+import Landing from '../pages/Landing';
+import Tasks from '../pages/Tasks';
 
 //I think the App component will render the different routes
+// the app componenet might not be the best place to render the NavBar because this will not be the same for all views
 
 export default function App(props) {
   return (
@@ -13,6 +15,7 @@ export default function App(props) {
       <NavBar/>
       <Switch>
         <Route exact path="/" component={Landing}/>
+        <Route path="/tasks" component={Tasks}/>
       </Switch>
       <Footer/>
     </div>
---
+++ src/pages/Tasks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Tasks.css'
+
+//we need to render the NavBar in this component so that the modified button can be used.
+
+export default function Tasks(props) {
+  return (
+    <div className="tasks-body">
+      <h1>Tasks Placeholder</h1>
+    </div>
+  );
+}
