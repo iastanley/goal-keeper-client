9a9fba5780ec620c3d209b61344bcb7a3bc706c4
iastanley <iasvks.craigslist@gmail.com>
2017-06-18 16:23:54 -0700

    implemented controlled TaskListItem component

--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
 class TaskListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: this.props.task.completed,
+      text: this.props.task.name
+    }
+  }
+
+  onCheckChange(checked) {
+    this.setState({ checked })
+  }
+
+  onInputChange(text) {
+    this.setState({text});
+  }
+
   render() {
     return (
       <li
         className="task-list-item"
         style={{border: '2px solid' + this.props.color}}>
-        <input checked={this.props.task.completed} type="checkbox"/>
-        <input type="text" value={this.props.task.name}/>
+        <input
+          onChange={event => this.onCheckChange(event.target.checked)}
+          checked={this.state.checked}
+          type="checkbox"/>
+        <input
+          type="text"
+          onChange={event => this.onInputChange(event.target.value)}
+          value={this.state.text}/>
         <i className="fa fa-lg fa-times" aria-hidden="true"></i>
       </li>
     );
