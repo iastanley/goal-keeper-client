17674aa599ba01a533021d09d792bbd20552be6b
iastanley <iasvks.craigslist@gmail.com>
2017-07-04 17:24:07 -0700

    added tests for EditGoalModal

--- src/components/__tests__/EditGoalModal.test.js
+++ src/components/__tests__/EditGoalModal.test.js
@@ -8,4 +8,45 @@ describe('EditGoalModal', () => {
     const wrapper = shallow(<EditGoalModal/>);
     expect(wrapper.hasClass('edit-goal-modal')).toBe(true);
   });
+
+  it('should render with initial state from goal', () => {
+    const goalToEdit =     {
+          _id: 1,
+          title: 'Goal 1',
+          color: '#e91e63',
+          tasks: [
+            {
+              _id: 10,
+              name: 'Do this first',
+              completed: true,
+              date: '2017-07-02'
+            },
+            {
+              _id: 11,
+              name: 'Do this second',
+              completed: false,
+              date: '2017-06-01'
+            }
+          ]
+        }
+    const wrapper = mount(
+      <EditGoalModal
+      goal={null}
+      show={true}
+      isLoading={false}
+      goalError={null}
+      close={jest.fn()}
+      editGoal={jest.fn()}
+      deleteGoal={jest.fn()}
+      setGoalError={jest.fn()}
+      />
+    );
+
+    wrapper.setProps({
+      goal: goalToEdit
+    });
+
+    expect(wrapper.state('color')).toEqual(goalToEdit.color);
+    expect(wrapper.state('title')).toEqual(goalToEdit.title);
+  });
 });
